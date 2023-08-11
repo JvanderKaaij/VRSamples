ea85b3010fc1d3a518bb3ccf72887f386576ee7f
Joey vd Kaaij <j.g.vanderkaaij@uva.nl>
2023-08-11 14:48:41 +0200

    Got VR Tracking with Vicon working

--- Assets/Scripts/TransformCopy.cs
+++ Assets/Scripts/TransformCopy.cs
@@ -4,13 +4,23 @@ public class TransformCopy : MonoBehaviour
 {
     [SerializeField] private Transform donor;
     [SerializeField] private Vector3 offsetRotation;
-    void Update()
+    [SerializeField] private Vector3 positionOffset;
+    [SerializeField] private bool trackPosition;
+    [SerializeField] private bool trackRotation;
+
+    private void Update()
     {
-        if (donor != null)
+        if (!donor) return;
+        
+        if (trackPosition)
         {
-            transform.position = donor.position;
+            var position = donor.position;
+            Vector3 newPosition = new Vector3(position.x + positionOffset.x, position.y + positionOffset.y, position.z + positionOffset.z);
+            transform.position = newPosition;
+        }
+        
+        if (trackRotation){
             var rotation = donor.rotation;
-            
             Vector3 newRotation = new Vector3(rotation.eulerAngles.z + offsetRotation.x,
                 rotation.eulerAngles.y + offsetRotation.y, -rotation.eulerAngles.x + offsetRotation.z);
             transform.rotation = Quaternion.Euler(newRotation);
